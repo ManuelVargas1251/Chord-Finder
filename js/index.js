0663558b732daa8a7be0eab705d393e481094c77
Manuel Vargas <manuelvargas@my.unt.edu>
2017-11-30 02:14:46 -0600

    finishing js layout

--- js/index.js
+++ js/index.js
@@ -1,39 +1,17 @@
 console.clear()
 
-//var _alphabet = []
-
 function get_user_chord(){
-	return ["D","F#","A"];
+	//return ["D","F#","A","C#"];
+	//return ["D","F","A","C"];
+	return ["C","E","G#","Bb"];
 }
 
-//returns what type of accidental the chord has; sharps, flats, or natural
-function get_accidental(user_chord){
-	
-	var accidental = "natural"
-	
-	user_chord.forEach(function(note){
-		if(note[1] === "#"){
-			accidental = "sharps"
-		}
-		else if(note[1] === "b"){
-			accidental = "flats"
-		}
-	});
-	
-	return accidental;
-};
+//needs to be re-written, 
+const _alphabet = ["A", ["A#", "Bb"], "B", "C", ["C#", "Db"], "D", ["D#", "Eb"], "E", "F", ["F#", "Gb"], "G", ["G#", "Ab"]];
 
-function get_user_alphabet(accidental){
-	if(accidental === "sharps"){
-		var AB = "A#", CD = "C#", DE = "D#", FG = "F#", GA = "G#";
-		}
-	else{
-		var AB = "Bb", CD = "Db", DE = "Eb", FG = "Gb", GA = "Ab";
-	}
-	return ["A", AB, "B", "C", CD, "D", DE, "E", "F", FG, "G", GA]
-}
-
-function get_alpha_root(user_chord, chord_size, alphabet){
+//return alphabet starting with the root of the chord
+function get_alphabet_from_root(user_chord, alphabet){
+	
 	let chord_root = user_chord[0]
 	var alpha_root = []
 	var alpha_temp = []
@@ -54,66 +32,57 @@ function get_alpha_root(user_chord, chord_size, alphabet){
 }
 
 function get_interval(alphabet, start_note, end_note){
-	//console.log("one int");
 	//console.log(start_note + " " + end_note)
 	
 	var index = 0;
 	var indexj = 0;
 	
-	while(alphabet[index] != start_note){
+	//traverse through alphabet if start note not reached yet
+	while(!alphabet[index].includes(start_note)){
 		index ++;
 	}
-	
-	while(alphabet[index] != end_note){
+	while(!alphabet[index].includes(end_note)){
 		//console.log(alphabet[index])
 		index++;
-		indexj++
+		indexj++;
 	}
-	console.log("index: " + indexj);
+	//console.log("index: " + indexj);
 	
 	return indexj+1;
 }
 
+//return array of intervals found in chord
 function get_intervals(user_chord, alphabet_from_root){
 	
-	let i = user_chord.length - 1;
+	var i = (user_chord.length) - 1;
 	var index = 0;
-	let intervals = [];
+	var intervals = [];
 	
 	while(i>0){
 		intervals.push(get_interval(alphabet_from_root, user_chord[index], user_chord[index+1]));
-		
-		//console.log("pla")
 		i -= 1;
 		index += 1;
 	}
-	
-	// using 5-limit tuning
-	// https://en.wikipedia.org/wiki/Interval_(music)#Interval_number_and_quality
-	console.log("Intervals: " + intervals)
+
+	return intervals
 }
 
 //////////////////
 //////////////////
-//MAIN MAIN MAIN M
+//main
+//////////////////
+//////////////////
 //instantly get input array
 let user_chord = get_user_chord()
-console.log(user_chord)
+console.log("user chord: " + user_chord)
 
-let accidental = get_accidental(user_chord)
-//console.log(accidental)
+//returns alphabet starting with the root of the chord
+let alphabet_from_root = get_alphabet_from_root(user_chord, _alphabet)
+console.log("alphabet: " + alphabet_from_root)
 
-//load either sharps or flats alphabet
-//maybe not needed
-var user_alphabet = get_user_alphabet(accidental) 
-//console.log(user_alphabet)
-
-//alphabet starting with the root of the
-let alphabet_from_root = get_alpha_root(user_chord, user_chord.length, user_alphabet)
-console.log(alphabet_from_root)
-
-//write function that organize notes that may be clustered A B C# E => A C# E B
+//write function to convert chord cluster to root position; A B C# E => A C# E B
 
 //stores array with all intervals of notes
 let intervals = get_intervals(user_chord, alphabet_from_root)
+console.log(intervals)
 
--- readme.md
+++ readme.md
@@ -11,40 +11,44 @@ Javascript web app fork from my c++ version
 
 ## Interval Reference
 
-| Interval       | Value|
-| -------------- |:----:|
-| Perfect Unison |	0, 1|
-| Minor second   |	2	|
-| Major second 	 |	3 	|
-| Minor third    |	4	|
-| Major third    |	5 	|
-| Perfect fourth |	6 	|
-| Augmented fourth |	7 |
-| Diminished fifth |	7 |
-| Perfect fifth    |	8 |
-| Minor sixth  	   |	9 |
-| Major sixth      |	10 |
-| Minor seventh    |	11 |
-| Major seventh    |	12 |
-| Perfect octave   |	13 |
+| Interval       	| Value	|
+| -------------- 	|:-----:|
+| Perfect Unison 	|	0, 1|
+| Minor Second   	|	2	|
+| Major Second 	 	|	3 	|
+| Minor Third    	|	4	|
+| Major Third	    |	5 	|
+| Perfect Fourth	|	6 	|
+| Augmented Fourth	|	7 	|
+| Diminished Fifth	|	7 	|
+| Perfect Fifth    	|	8 	|
+| Minor Sixth  	   	|	9 	|
+| Major Sixth     	|	10 	|
+| Minor Seventh   	|	11 	|
+| Major Seventh   	|	12 	|
+| Perfect Octave  	|	13 	|
 
 ## Chord Formation
 4 Triads, 8 Tetrads 
 
-| Chord Name			| Interval Group		|	Chord Type	|
-| ----------			| --------------		|:-----------------:|
-|	Major				|	```[5, 4]```		| 	Triad	|
-|	Minor				|	```[4, 5]```		| 	Triad	|
-|	Augmented			|	```[5, 5]```		| 	Triad	|
-|	Diminished			|	```[4, 4]```		| 	Triad	|
-|	Major 7th			|	```[5, 4, 5]```		|	Tetrad	|
-|	Minor 7th			|	```[4, 5, 4]```		|	Tetrad	|
-|	Augmented 7th		|	```[5, 5, 3]```		|	Tetrad	|
-|	Dominant 7th		|	```[5, 4, 4]```		|	Tetrad	|
-|	Augmented Major 7th	|	```[5, 5, 4]```		|	Tetrad	|
-|	Diminished 7th		|	```[4, 4, 4]```		|	Tetrad	|
-|	Half-Diminished 	|	```[4, 4, 5]```		|	Tetrad	|
-|	Minor Major 		|	```[4, 5, 5]```		|	Tetrad	|
+| Chord Name			| Interval Group			|	Chord Type	|
+| ----------			| --------------			|:-----------------:|
+|	Major				|	```[5, 4]```			| 	Triad	|
+|	Minor				|	```[4, 5]```			| 	Triad	|
+|	Augmented			|	```[5, 5]```			| 	Triad	|
+|	Diminished			|	```[4, 4]```			| 	Triad	|
+|	Major 7th			|	```[5, 4, 5]```			|	Seventh	|
+|	Minor 7th			|	```[4, 5, 4]```			|	Seventh	|
+|	Augmented 7th		|	```[5, 5, 3]```			|	Seventh	|
+|	Dominant 7th		|	```[5, 4, 4]```			|	Seventh	|
+|	Augmented Major 7th	|	```[5, 5, 4]```			|	Seventh	|
+|	Diminished 7th		|	```[4, 4, 4]```			|	Seventh	|
+|	Half-Diminished 	|	```[4, 4, 5]```			|	Seventh	|
+|	Minor Major 		|	```[4, 5, 5]```			|	Seventh	|
+|	Major 9th			|	```[5, 4, 5, 4]```		|	Extended 	|
+|	Dominant 9th		|	```[5, 4, 4, 5]```		|	Extended 	|
+|	Dominant 11th		|	```[5, 4, 4, 5, 4]```	|	Extended 	|
+|	Dominant 13th		|	```[5, 4, 4, 5, 4, 5]```|	Extended 	|
 
 
 # Reference
