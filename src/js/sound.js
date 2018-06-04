e82b638c81c68119cb542934ce35cf98154dc1c2
Manuel Vargas <manuelvargas1251@gmail.com>
2018-06-03 22:52:41 -0500

    0.6.7 🍖

--- src/js/sound.js
+++ src/js/sound.js
@@ -1,11 +1,74 @@
-createjs.FlashAudioPlugin.swfPath = "../src/soundjs/flashaudio";
-createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.FlashAudioPlugin]);
-createjs.Sound.alternateExtensions = ["mp3"];
-createjs.Sound.on("fileload", this.loadHandler, this);
-createjs.Sound.registerSound("path/to/mySound.ogg", "sound");
+console.log('sound-start');
+
+
+var soundID = "Thunder"
+
+function loadSound() {
+    createjs
+        .Sound
+        .registerSound("../sound/mySound.mp3", soundID)
+}
+
+
+function playSound() {
+    createjs
+        .Sound
+        .play(soundID)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//createjs.FlashAudioPlugin.swfPath = "src/soundjs/flashaudio";
+/*
+createjs
+    .Sound
+    .registerPlugins([
+        createjs.WebAudioPlugin,
+        createjs.FlashAudioPlugin
+    ]);
+
+createjs
+    .Sound
+    .alternateExtensions = ["mp3"];
+
+createjs
+    .Sound
+    .on("fileload", this.loadHandler, this);
+
+createjs
+    .Sound
+    .registerSound("src/sound/mySound.wav", "sound", 2);
+
+
+
+
+
+
+
+
+
 function loadHandler(event) {
     // This is fired for each sound that is registered.
     var instance = createjs.Sound.play("sound");  // play using id.  Could also use full source path or event.src.
     instance.on("complete", this.handleComplete, this);
     instance.volume = 0.5;
 }
+*/
+console.log('sound-end');
