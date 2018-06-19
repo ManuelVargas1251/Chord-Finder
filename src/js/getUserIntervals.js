9558a84fd3f1ed2f997a0db095242d8d5619e631
Manuel Vargas <manuelvargas1251@gmail.com>
2018-06-18 23:41:52 -0500

    0.6.9 🥧

--- src/js/getUserIntervals.js
+++ src/js/getUserIntervals.js
@@ -1,10 +1,6 @@
 //return array of intervals found in chord
-function getUserIntervals(userChord, alphabet_from_root) {
-
-	console.log("user chord notesum: " + userChord.length)
-	// userChord.forEach(element => {
-	// 	console.log(getNoteId(element))
-	// });
+function getUserIntervals(userChord) {
+	console.log("userChord Length: " + userChord.length)
 
 	let num_of_intervals = userChord.length - 1,
 		index = 0,
@@ -12,34 +8,7 @@ function getUserIntervals(userChord, alphabet_from_root) {
 		interval
 
 	while (num_of_intervals > 0) {
-
-		let note_one = userChord[index]
-			, note_two = userChord[index + 1]
-
-		console.log(
-			'1: ' 		+ 
-			note_one 	+
-			', 2: ' 	+ 
-			note_two
-		)
-
-		if (getNoteId(note_one) < getNoteId(note_two)) {
-			console.error('note ' + note_one + ' is to the left of note ' + note_two)
-		}
-		else {
-			console.error('note ' + note_one + ' is to the right of note ' + note_two)
-		}
-
-		interval = Math.abs(getNoteId(note_one) - getNoteId(note_two)) + 1
-		console.log('interval:: ' + interval)
-		intervals.push(interval)
-		// intervals.push(
-		// 	getInterval(
-		// 		alphabet_from_root,
-		// 		userChord[index],
-		// 		userChord[index + 1]
-		// 	)
-		// )
+		intervals.push(getInterval(userChord[index],userChord[index + 1]))
 		num_of_intervals--
 		index++
 	}
--- src/js/updateChord.js
+++ src/js/updateChord.js
@@ -1,13 +1,13 @@
 // main function in the program
 function updateChord(newChord) {
 
-		// returns alphabet starting with the root of the chord
-		// console.log("alphabet: " + alphabet_from_root)
-	let alphabetFromRoot = getAlphabetFromRoot(newChord),
+	// returns alphabet starting with the root of the chord
+	// console.log("alphabet: " + alphabet_from_root)
+	let alphabetFromRoot = getAlphabetFromRoot(newChord)
 
-		// stores array with all intervals of notes
-		// console.log(intervals)
-		userIntervals = getUserIntervals(newChord, alphabetFromRoot)
+	// stores array with all intervals of notes
+	// console.log(intervals)
+	let userIntervals = getUserIntervals(newChord, alphabetFromRoot)
 
 	// find chord using the chord letters and interval values
 	return getChord(newChord, userIntervals)
