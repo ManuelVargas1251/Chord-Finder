9aa22b8f7257503a252de7dd0a6ca8dc4efba956
Manuel Vargas <manuelvargas1251@gmail.com>
2018-06-19 00:19:51 -0500

    0.6.10 🍣

--- src/js/sound.js
+++ src/js/sound.js
@@ -1,74 +1,15 @@
 console.log('sound-start');
 
 
-var soundID = "Thunder"
+var sound = new Howl({
+    src: ['src/sound/mySound.mp3'],
+    autoplay: true,
+    onend: function () {
+        console.log('Finished!');
+    }
+});
 
-function loadSound() {
-    createjs
-        .Sound
-        .registerSound("../sound/mySound.mp3", soundID)
-}
-
-
-function playSound() {
-    createjs
-        .Sound
-        .play(soundID)
-}
+sound.play();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//createjs.FlashAudioPlugin.swfPath = "src/soundjs/flashaudio";
-/*
-createjs
-    .Sound
-    .registerPlugins([
-        createjs.WebAudioPlugin,
-        createjs.FlashAudioPlugin
-    ]);
-
-createjs
-    .Sound
-    .alternateExtensions = ["mp3"];
-
-createjs
-    .Sound
-    .on("fileload", this.loadHandler, this);
-
-createjs
-    .Sound
-    .registerSound("src/sound/mySound.wav", "sound", 2);
-
-
-
-
-
-
-
-
-
-function loadHandler(event) {
-    // This is fired for each sound that is registered.
-    var instance = createjs.Sound.play("sound");  // play using id.  Could also use full source path or event.src.
-    instance.on("complete", this.handleComplete, this);
-    instance.volume = 0.5;
-}
-*/
 console.log('sound-end');
