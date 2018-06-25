714f54d77cf170068172ab644d2871a506b03feb
Manuel Vargas <manuelvargas1251@gmail.com>
2018-06-24 22:17:52 -0500

    0.6.44 🍉

--- src/js/getUserIntervals.js
+++ src/js/getUserIntervals.js
@@ -1,6 +1,6 @@
 //return array of intervals found in chord
 function getUserIntervals(userChord) {
-	console.log("userChord Length: " + userChord.length)
+	//console.log("userChord Length: " + userChord.length)
 
 	let index = 0,
 		intervals = [],
@@ -9,14 +9,24 @@ function getUserIntervals(userChord) {
 	// store intervals
 	while (num_of_intervals > 0) {
 		intervals.push(
-			getInterval(
-				getNoteId(userChord[index]),
-				getNoteId(userChord[index + 1])
-			)
+			// getInterval(
+			// 	getNoteId(userChord[index]),
+			// 	getNoteId(userChord[index + 1])
+			// )
+
+			Math.abs(
+				getNoteId(userChord[index]) -
+				getNoteId(userChord[index + 1]))
+				 + 1
 		)
 		num_of_intervals--
 		index++
 	}
 	return intervals
+}
+function getNoteId(value) {
+    return Object
+        .keys(_notes)
+        .find(key => _notes[key] === value);
 }
 module.exports = getUserIntervals
--- tests/updateChord.test.js
+++ tests/updateChord.test.js
@@ -7,3 +7,19 @@ test('return chord name from chord notes', () => {
   expect(updateChord(userChord))
     .toEqual('C Major 7')
 })
+
+
+
+// test('return intervals from user chord', () => {
+//   expect(getUserIntervals(['E', 'G#', 'B'], _alphabet))
+//     .toEqual([5, 4])
+// })
+
+// expect(getUserIntervals(['E', 'G#', 'B'], _alphabet))
+// .toEqual([5, 4])
+
+// expect(getChord(['C', 'E', 'G', 'B'], [5, 4, 5]))
+// .toEqual('C Major 7')
+
+// expect(getNoteId('C'))
+// .toEqual('0')
