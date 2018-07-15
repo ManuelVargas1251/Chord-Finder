0919fd2aa593fd1637b3a4d190fb60d75e9ad3a2
Manuel Vargas <manuelvargas1251@gmail.com>
2018-07-15 14:21:57 -0500

    0.7.13 🍍

--- src/js/sound.js
+++ src/js/sound.js
@@ -1,13 +1,27 @@
+var notes = []
+
+for (i = 0; i < 12; i++) {
+    notes[i] = new Howl({
+        src: ['src/sound/' + i + '.wav'],
+        loop: false,
+        preload: true
+    });
+    console.warn('notes:' + notes[0])
+}
+
 function playNote(NoteId) {
-        let sound = new Howl({
-            src: ['src/sound/' + NoteId + '.wav'],
-            loop: false,
-            onend: function () {
-                console.log('Finished!');
-            },
-            onloaderror: function () {
-                console.error('no sound file found')
-            }
-        }).play()
-        console.log('sound: ' + sound)
+    // let sound = new Howl({
+    //     src: ['src/sound/' + NoteId + '.wav'],
+    //     loop: false,
+    //     onend: function () {
+    //         console.log('Finished!');
+    //     },
+    //     onloaderror: function () {
+    //         console.error('no sound file found')
+    //     }
+    // }).play()
+
+    notes[NoteId].play()
+
+    console.log('sound: ' + sound)
 }
