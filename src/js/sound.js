b85c4b52e3cb74dd15c84e8b1f675b00dcdb98fa
Manuel Vargas <manuelvargas1251@gmail.com>
2018-07-17 22:07:36 -0500

    0.7.28 🍍

--- src/js/sound.js
+++ src/js/sound.js
@@ -1,22 +1,40 @@
-const Howl = require('./external/howler.min.js').Howl
-let notes = []
+//const Howl = require('').Howl
+//let Howl = require('./external/Howler.min').Howl
 
-//preloading notes files
-for (i = 0; i < 12; i++) {
-    notes[i] = new Howl({
-        src: [
-            'src/sound/mp3/' + i + '.mp3',
-            'src/sound/wav/' + i + '.wav'
-        ],
-        loop: false,
-        preload: true
-    });
-    //console.log('notes:' + notes[0])
+let preloaded = false
+
+preload()
+
+function preload() {
+    //new Howl();
+    //preloading notes files
+    for (i = 0; i < 12; i++) {
+        notes[i] = new Howl({
+            src: [
+                'src/sound/mp3/' + i + '.mp3',
+                'src/sound/wav/' + i + '.wav'
+            ],
+            loop: false,
+            preload: true
+        });
+        //console.log('notes:' + notes[0])
+    }
+    return true
 }
 
 //plays note when pressed/clicked
 function playNote(NoteId, notes) {
-    notes[NoteId].play()
+    if (preloaded!=undefined) {
+        try {
+            notes[NoteId].play()
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    else{
+        console.log('not preloaded')
+    }
     return notes[NoteId]
 }
 
