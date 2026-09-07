7740f0051ae2ccb7025572ce5c2a7b82b8911bfb
ManuelVargas1251 <ManuelVargas1251@users.noreply.github.com>
2026-09-07 21:35:56 +0000

    Deploying to gh-pages from @ ManuelVargas1251/Chord-Finder@e74d38a317eb32b9edcf4d1cc437cff7bc0116cd 🚀

--- pr-preview/pr-83/src/js/getUserIntervals.test.js
+++
@@ -1,6 +0,0 @@
-const getUserIntervals = require('./getUserIntervals')
-
-test('return chord name from chord notes', () => {
-    expect(getUserIntervals(['E', 'G#', 'B'], _alphabet))
-        .toEqual([5, 4])
-})
