67e3220f8d06382f35aa155e7a65f101e32ef92e
ManuelVargas1251 <ManuelVargas1251@users.noreply.github.com>
2026-09-14 21:03:41 +0000

    Deploying to gh-pages from @ ManuelVargas1251/Chord-Finder@33220c51b420ca118de0c6fa3b1b7d2de353fe0d 🚀

---
+++ pr-preview/pr-99/src/js/getChord.test.js
@@ -0,0 +1,33 @@
+const getChord = require('./getChord').getChord
+
+test('returns chord names for supported note and interval combinations', () => {
+    expect(getChord(['C', 'E', 'G'], [5, 4]))
+        .toEqual('C Major')
+
+    expect(getChord(['C', 'Eb', 'G'], [4, 5]))
+        .toEqual('C Minor')
+
+    expect(getChord(['E', 'G', 'C'], [4, 6]))
+        .toEqual('C 1st Inversion')
+
+    expect(getChord(['G', 'C', 'E'], [6, 5]))
+        .toEqual('C 2nd Inversion')
+
+    expect(getChord(['C', 'E', 'G', 'B'], [5, 4, 5]))
+        .toEqual('C Major 7')
+})
+
+test('returns an empty string when no notes are selected', () => {
+    expect(getChord([], [])).toEqual('')
+})
+
+test('returns an empty string when the interval combination is unsupported', () => {
+    const consoleWarn = jest.spyOn(console, 'warn').mockImplementation(() => {})
+
+    try {
+        expect(getChord(['C', 'C#', 'D'], [2, 2])).toEqual('')
+        expect(consoleWarn).toHaveBeenCalledWith('no chord defined')
+    } finally {
+        consoleWarn.mockRestore()
+    }
+})
