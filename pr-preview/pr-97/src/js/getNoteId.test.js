ecee9da2260dd8e922adeda5a6910e54752fe2b6
ManuelVargas1251 <ManuelVargas1251@users.noreply.github.com>
2026-09-19 16:43:14 +0000

    Deploying to gh-pages from @ ManuelVargas1251/Chord-Finder@f0eae26b0598a8c1bf3738c69c91a75bded83a82 🚀

--- pr-preview/pr-97/src/js/getNoteId.test.js
+++ pr-preview/pr-97/src/js/getNoteId.test.js
@@ -1,6 +1,10 @@
 const getNoteId = require('./getNoteId')
 
 describe('getNoteId Suite', () => {
+  test('returns undefined when the note is not found', () => {
+    expect(getNoteId('T')).toBeUndefined()
+  })
+
   test('returns the string ID for natural notes', () => {
     expect(getNoteId('C')).toEqual('0')
     expect(getNoteId('E')).toEqual('4')
@@ -10,9 +14,5 @@ describe('getNoteId Suite', () => {
   test('returns the string ID for sharp notes', () => {
     expect(getNoteId('C#')).toEqual('1')
     expect(getNoteId('A#')).toEqual('10')
-  })
-
-  test('returns undefined when the note is not found', () => {
-    expect(getNoteId('T')).toBeUndefined()
   })
 })
